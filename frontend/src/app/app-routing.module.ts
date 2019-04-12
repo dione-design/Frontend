9b4489d581379f36eddc9a430998a2fd270c7f8f
Prabhani Nanayakkara <prabhaninanayakkara1907@gmail.com>
2019-04-12 20:10:08 +0530

    added home-page routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home-page', component: HomePageComponent },
+  { path: '', redirectTo: '/home-page', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
