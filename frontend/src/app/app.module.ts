088f270e4bfbec0e9c54154375e68a7dbc480a48
Prabhani Nanayakkara <prabhaninanayakkara1907@gmail.com>
2019-04-12 20:09:18 +0530

    created and updated home-page component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModuleModule } from './material-module/material-module.module';
+import { HomePageComponent } from './home-page/home-page.component';
 // import {FormControl, Validators} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomePageComponent,
   ],
   imports: [
     BrowserModule,
