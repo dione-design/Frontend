e43f57eeccda067edb74d961c05cd5e488618004
Prabhani Nanayakkara <prabhaninanayakkara1907@gmail.com>
2019-04-13 13:31:36 +0530

    Added signup-page routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { HomewearComponent } from './homewear/homewear.component';
 import { NewArrivalsComponent } from './new-arrivals/new-arrivals.component';
 import { OffersComponent } from './offers/offers.component';
 import { BestSellingComponent } from './best-selling/best-selling.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
 
 const routes: Routes = [
   { path: 'home-page', component: HomePageComponent },
@@ -19,6 +20,7 @@ const routes: Routes = [
   { path: 'new-arrivals', component: NewArrivalsComponent},
   { path: 'offers', component: OffersComponent},
   { path: 'best-selling', component: BestSellingComponent},
+  { path: 'signup-page', component: SignupPageComponent},
 ];
 
 @NgModule({
