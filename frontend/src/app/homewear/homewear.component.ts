f6f531d2fe15aff80b3bb6e3e97a36404e54a661
Prabhani Nanayakkara <prabhaninanayakkara1907@gmail.com>
2019-04-13 12:45:35 +0530

    created and updated homewear component

---
+++ frontend/src/app/homewear/homewear.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homewear',
+  templateUrl: './homewear.component.html',
+  styleUrls: ['./homewear.component.scss']
+})
+export class HomewearComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
