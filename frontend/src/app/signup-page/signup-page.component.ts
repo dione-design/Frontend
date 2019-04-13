b858d24d317150fe4a0b85d395cd0a2671ad83e1
Prabhani Nanayakkara <prabhaninanayakkara1907@gmail.com>
2019-04-13 23:55:40 +0530

    Updated sign up forms

--- frontend/src/app/signup-page/signup-page.component.ts
+++ frontend/src/app/signup-page/signup-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-signup-page',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup-page.component.scss']
 })
 export class SignupPageComponent implements OnInit {
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+        this.email.hasError('email') ? 'Not a valid email' :
+            '';
+  }
+
+  hide = true;
 
   constructor() { }
 
