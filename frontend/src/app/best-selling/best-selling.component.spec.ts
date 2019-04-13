705966e3379bc463f82e93d85ea4888df3e1f429
Prabhani Nanayakkara <prabhaninanayakkara1907@gmail.com>
2019-04-13 13:07:09 +0530

    created and updated best-selling component

---
+++ frontend/src/app/best-selling/best-selling.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BestSellingComponent } from './best-selling.component';
+
+describe('BestSellingComponent', () => {
+  let component: BestSellingComponent;
+  let fixture: ComponentFixture<BestSellingComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BestSellingComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BestSellingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
